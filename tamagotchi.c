ccaff389387564f039ddedc7c369e77047fc5ae0
GemsZen <GemsZen@users.noreply.github.com>
2023-07-31 10:15:45 -0300

    tama

--- tamagotchi.c
+++ tamagotchi.c
@@ -2,25 +2,94 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <unistd.h>
+#include <time.h>
+
+#ifdef _WIN32
+#include <conio.h>
+#else
+#include <sys/select.h>
+#include <termios.h>
+#include <stropts.h>
+#include <sys/ioctl.h>
+
+int kbhit() {
+    struct timeval tv;
+    fd_set fds;
+    tv.tv_sec = 0;
+    tv.tv_usec = 0;
+    FD_ZERO(&fds);
+    FD_SET(STDIN_FILENO, &fds);
+    select(STDIN_FILENO + 1, &fds, NULL, NULL, &tv);
+    return FD_ISSET(STDIN_FILENO, &fds);
+}
+#endif
 
 typedef struct {
     char name[20];
     int happiness;
     int hunger;
+    char* asciiArt;
 } Tamagotchi;
 
 void initialize(Tamagotchi* tamagotchi, const char* name) {
     strcpy(tamagotchi->name, name);
     tamagotchi->happiness = 50;
     tamagotchi->hunger = 50;
+
+    while (!kbhit()) {
+        tamagotchi->asciiArt = "    /\\_/\\\n"
+                               "   ( -.- )\n"
+                               "    > ^ <";
+        displayTamagotchi(tamagotchi);
+        usleep(90000); // Pausa por 0.5 segundos
+        tamagotchi->asciiArt = "    /\\_/\\\n"
+                               "   ( o.o )\n"
+                               "    > ^ <";
+        displayTamagotchi(tamagotchi);
+        sleep(3); // Pausa por 0.5 segundos
+    }
 }
+
+
+void clearScreen() {
+    printf("\033[H\033[J"); // Cï¿½digo ANSI para limpar a tela
+}
+
+
+void displayTamagotchi(Tamagotchi* tamagotchi) {
+    clearScreen();
+    printf("Nome: %s\n", tamagotchi->name);
+    printf("Fome: %d/100 - Felicidade: %d/100\n", tamagotchi->hunger, tamagotchi->happiness);
+    printf("Aparï¿½ncia:\n%s\n", tamagotchi->asciiArt);
+    printf("Comandos disponï¿½veis:\n");
+    printf("- feed: Alimentar o %s\n", tamagotchi->name);
+    printf("- pet: Dar carinho ao %s\n", tamagotchi->name);
+    printf("- walk: Levar o %s para passear\n", tamagotchi->name);
+    printf("- exit: Deixar %s descansar\n", tamagotchi->name);
+}
+
 
 void feed(Tamagotchi* tamagotchi) {
     tamagotchi->hunger -= 10;
     if (tamagotchi->hunger < 0) {
         tamagotchi->hunger = 0;
     }
-    printf("%s foi alimentado!\n", tamagotchi->name);
+    time_t startTime = time(NULL);
+    while (time(NULL) - startTime < 10 && !kbhit()) {
+        tamagotchi->asciiArt = "    /\\_/\\\n"
+                               "   ( -.- )\n"
+                               "    > w <";
+        displayTamagotchi(tamagotchi);
+        usleep(200000); // Pausa por 0.2 segundos
+        tamagotchi->asciiArt = "    /\\_/\\\n"
+                               "   ( -.- )\n"
+                               "    > m <";
+        displayTamagotchi(tamagotchi);
+        usleep(200000); // Pausa por 0.2 segundos
+    }
+
+    displayTamagotchi(tamagotchi);
 }
 
 void pet(Tamagotchi* tamagotchi) {
@@ -28,7 +97,21 @@ void pet(Tamagotchi* tamagotchi) {
     if (tamagotchi->happiness > 100) {
         tamagotchi->happiness = 100;
     }
-    printf("%s teve seu pelo alisado!\n", tamagotchi->name);
+    time_t startTime = time(NULL);
+    while (time(NULL) - startTime < 5 && !kbhit()) {
+        tamagotchi->asciiArt = "    /\\_/\\\n"
+                               "    (^?^)\n"
+                               "    > v <";
+        displayTamagotchi(tamagotchi);
+        usleep(100000); // Pausa por 0.1 segundos
+        tamagotchi->asciiArt = "    /\\_/\\\n"
+                               "    (^?^)\n"
+                               "    > ^ <";
+        displayTamagotchi(tamagotchi);
+        usleep(100000); // Pausa por 0.1 segundos
+    }
+
+    displayTamagotchi(tamagotchi);
 }
 
 void walk(Tamagotchi* tamagotchi) {
@@ -40,38 +123,22 @@ void walk(Tamagotchi* tamagotchi) {
     if (tamagotchi->hunger > 100) {
         tamagotchi->hunger = 100;
     }
-    printf("%s foi passear!\n", tamagotchi->name);
-}
-
-void changeEmoji(Tamagotchi* tamagotchi) {
-    printf("%s mudou de emoji!\n", tamagotchi->name);
-}
-
-void checkStatus(Tamagotchi* tamagotchi) {
-    printf("Fome: %d/100 - Felicidade: %d/100\n", tamagotchi->hunger, tamagotchi->happiness);
-    if (tamagotchi->hunger >= 100) {
-        printf("%s morreu de fome. Game over!\n", tamagotchi->name);
-        exit(0);
-    } else if (tamagotchi->happiness >= 100) {
-        printf("%s está extremamente feliz. Parabéns!\n", tamagotchi->name);
-        exit(0);
+    time_t startTime = time(NULL);
+    while (time(NULL) - startTime < 10 && !kbhit()) {
+        tamagotchi->asciiArt = "    /\\_/\\\n"
+                               "   ( u.u )\n"
+                               "    > D <";
+        displayTamagotchi(tamagotchi);
+        usleep(300000); // Pausa por 0.3 segundos
+        tamagotchi->asciiArt = "    /\\_/\\\n"
+                               "   ( o.o )\n"
+                               "    > O <";      
+                               
+        displayTamagotchi(tamagotchi);
+        usleep(300000); // Pausa por 0.3 segundos
     }
-}
 
-void start(Tamagotchi* tamagotchi) {
-    while (true) {
-        tamagotchi->happiness -= 10;
-        tamagotchi->hunger += 10;
-        if (tamagotchi->happiness < 0) {
-            tamagotchi->happiness = 0;
-        }
-        if (tamagotchi->hunger > 100) {
-            tamagotchi->hunger = 100;
-        }
-        printf("Tempo passando...\n");
-        checkStatus(tamagotchi);
-        sleep(10);
-    }
+    displayTamagotchi(tamagotchi);
 }
 
 int main() {
@@ -80,41 +147,36 @@ int main() {
 
     printf("Digite o nome do seu Tamagotchi: ");
     fgets(name, sizeof(name), stdin);
-    name[strcspn(name, "\n")] = '\0';
+    name[strcspn(name, "\n")] = '\0'; // Remove o caractere de nova linha do final
 
     initialize(&tamagotchi, name);
 
-    printf("Um novo Tamagotchi chamado %s foi criado!\n", tamagotchi.name);
-    checkStatus(&tamagotchi);
-
-    printf("Comandos disponíveis:\n");
-    printf("- feed: Alimentar o Tamagotchi\n");
-    printf("- pet: alisar o pelo Tamagotchi\n");
-    printf("- walk: Levar o Tamagotchi para passear\n");
-    printf("- change: Mudar o emoji do Tamagotchi\n");
-    printf("- exit: Encerrar o programa\n");
-
     while (true) {
+        clearScreen();
+        displayTamagotchi(&tamagotchi);
+
         char command[10];
         printf("Digite um comando: ");
         fgets(command, sizeof(command), stdin);
-        command[strcspn(command, "\n")] = '\0';
+        command[strcspn(command, "\n")] = '\0'; // Remove o caractere de nova linha do final
 
         if (strcmp(command, "feed") == 0) {
             feed(&tamagotchi);
+            initialize(&tamagotchi, tamagotchi.name);
         } else if (strcmp(command, "pet") == 0) {
             pet(&tamagotchi);
+            initialize(&tamagotchi, tamagotchi.name);
         } else if (strcmp(command, "walk") == 0) {
             walk(&tamagotchi);
-        } else if (strcmp(command, "change") == 0) {
-            changeEmoji(&tamagotchi);
+            initialize(&tamagotchi, tamagotchi.name);
         } else if (strcmp(command, "exit") == 0) {
             break;
         } else {
-            printf("Comando inválido.\n");
+            printf("Comando invï¿½lido.\n");
         }
     }
 
     return 0;
 }
+
 
